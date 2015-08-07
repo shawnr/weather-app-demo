4820ebd2cde6397cb8873d14b943178755a95c8e
Shawn Rider <shawn@shawnrider.com>
2015-08-07 10:49:42 -0700

    Initial commit.
    Change-Id: I5858433d88bc2e31ace2680e77b5b0654b86f2db

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the weatherApp
+ */
+angular.module('weatherApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherApp
+ */
+angular.module('weatherApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
